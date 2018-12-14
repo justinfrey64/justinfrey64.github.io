f7152f7bfe60c5970fc6a20dd00e930059817f4b
Justin Frey <justinfrey64@gmail.com>
2018-12-14 13:53:17 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#typed', {
+    strings:["This is a new webpage","This is under construction", "This is gunna be good"],
+    smartBackspace: true,
+    typeSpeed:50,
+    backSpeed:50,
+    backDelay:1000,
+    loop:true,
+    loopCount:Infinity
+});
